d44f389b3058c933c5b51ac309b69c5bdc18a905
nagakuta <nagakuta0314+github@gmail.com>
2023-12-12 21:44:33 +0900

    :wrench: Add sudo_local that use touch id when sudo

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -6,6 +6,7 @@ for dotfile in "${SCRIPT_DIR}"/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/bin/.config/system/sudo_local" ]] && continue
 
     ln -fnsv "$dotfile" "$HOME"
 done
